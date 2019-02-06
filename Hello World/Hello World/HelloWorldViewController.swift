12591b2e69684f88ee0d6c196c32983bbf90bb83
zjhww8 <zjhww8@mail.missouri.edu>
2019-02-06 16:07:23 -0600

    Add files via upload
    Hello World Swift Application.

---
+++ Hello World/Hello World/HelloWorldViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HelloWorldViewController.swift
+//  Hello World
+//
+//  Created by Zachary Hess on 2/6/19.
+//  Copyright © 2019 Zachary Hess. All rights reserved.
+//
+
+import UIKit
+
+class HelloWorldViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doHello(_ sender: UIButton) {
+        messageLabel.text = "Hello World!"
+    }
+    
+    @IBAction func doClear(_ sender: UIButton) {
+        messageLabel.text = ""
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
